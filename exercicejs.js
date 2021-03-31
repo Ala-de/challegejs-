3fb87234ddcb60928ef5b303eafe67fa6a493664
Ala-de <80680818+Ala-de@users.noreply.github.com>
2021-03-31 09:02:48 +0100

    Add files via upload

---
+++ exercicejs.js
@@ -0,0 +1,56 @@
+//exe1
+function sayHello(){
+    return"ala"
+}
+console.log('exercice',sayHello())
+//exe2
+var tab= [1, 2, 3,4];
+tab.reverse();
+
+console.log(tab) 
+//exe3
+function somme(n1, n2)
+
+  {
+  return ( n1 + n2)
+       // Calcul de la somme.
+       
+}
+console.log("la somme de a+b",somme(6, 10)) 
+let Nom='toto'
+let age='30'
+let homme='true'
+console.log(Nom,age,homme)
+//exe3
+function puicence(n)
+
+  {
+  return ( n *n)
+       // Calcul de la somme.
+       
+}
+console.log("la puicence est",puicence(6)) 
+function triangle(h,l)
+
+  {
+  return ( h*l/2)
+       // Calcul de la somme.
+       
+}
+console.log("la surface est",triangle(4,6)) 
+//ex 6
+
+
+
+console.log("le maximum et",Math.max(4,6,10))
+
+var tab= [14, 12, 23,44];
+console.log(tab[0]) 
+console.log(12 % 5);
+var tab= [11, 32, 23,15];
+tab.reverse();
+console.log(tab)
+var tab= [14, 12, 23,44];
+console.log(tab[tab.length - 1]); 
+let a:
+
